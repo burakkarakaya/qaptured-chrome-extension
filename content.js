e0c61953d6bd855fccf899433faa825f43cd0323
burakkarakaya <Brk@BlackRocks-iMac.local>
2020-05-26 13:23:42 +0300

    update
    netflix işinin alınması

--- content.js
+++ content.js
@@ -1,6 +1,72 @@
 
 const MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
     utils = {
+        ajx: function (o, callback) {
+
+            o = o || {};
+            var uri = o['uri'] || '',
+                type = o['type'] || 'json', // json ve html değerlerini alır. default değeri json
+                method = o['method'] || 'POST',
+                headers = o['headers'] || { 'Content-Type': 'application/json' },
+                data = o['data'] || {},
+                _callback = function (res) {
+                    if (typeof callback !== 'undefined')
+                        callback(res);
+                };
+
+            if (uri == '') {
+                _callback({ type: 'error' });
+                return false;
+            }
+
+
+            switch (type) {
+
+                case 'json':
+                    return fetch(uri, {
+                        method: method,
+                        headers: headers,
+                        body: JSON.stringify(data)
+                    })
+                        .then(res => res.json())
+                        .then(res => {
+                            if (res.IsSuccess)
+                                _callback({ type: 'success', data: res });
+                            else
+                                _callback({ type: 'error', message: res.Message });
+
+                        })
+                        .catch(error => {
+                            _callback({ type: 'error', message: error });
+                        });
+
+                case 'html': {
+                    var headers = {};
+                    return fetch(uri)
+                        .then(res => {
+                            headers = res.headers || {};
+                            return res.text();
+                        })
+                        .then(function (html) {
+                            try {
+                                var parser = new DOMParser();
+                                var doc = parser.parseFromString(html, 'text/html');
+
+                                _callback({ type: 'success', data: html, doc: doc, headers: headers || {} });
+
+                            } catch (error) {
+                                _callback({ type: 'error', message: error.message });
+                            }
+
+                        }).catch(function (error) {
+                            _callback({ type: 'error', message: error });
+                        });
+                }
+
+                default:
+                    break;
+            }
+        },
         detectEl: function (elm) {
             return elm == null ? false : true;
         },
@@ -16,21 +82,109 @@ const MutationObserver = window.MutationObserver || window.WebKitMutationObserve
                 cls = (o['cls'] || '').replace(/\./g, '');
 
             return classList.contains(cls) || false;
-        }
+        },
+        getParents: function (elem, selector) {
+
+            /* 
+                https://github.com/happyBanshee/JS-helpers/wiki/.closest(),-.parents(),-.parentsUntil(),-.find()-in-JS
+    
+                
+                var elem = document.querySelector('#some-element');
+                utils.getParents(elem, '.some-class');
+                utils.getParents(elem.parentNode, '[data-product-id]');
+    
+            */
+
+            // Variables
+            var firstChar = selector.charAt(0);
+            var supports = 'classList' in document.documentElement;
+            var attribute, value;
+
+            // If selector is a data attribute, split attribute from value
+            if (firstChar === '[') {
+                selector = selector.substr(1, selector.length - 2);
+                attribute = selector.split('=');
+
+                if (attribute.length > 1) {
+                    value = true;
+                    attribute[1] = attribute[1].replace(/"/g, '').replace(/'/g, '');
+                }
+            }
+
+            // Get closest match
+            for (; elem && elem !== document && elem.nodeType === 1; elem = elem.parentNode) {
+
+                // If selector is a class
+                if (firstChar === '.') {
+                    if (supports) {
+                        if (elem.classList.contains(selector.substr(1))) {
+                            return elem;
+                        }
+                    } else {
+                        if (new RegExp('(^|\\s)' + selector.substr(1) + '(\\s|$)').test(elem.className)) {
+                            return elem;
+                        }
+                    }
+                }
+
+                // If selector is an ID
+                if (firstChar === '#') {
+                    if (elem.id === selector.substr(1)) {
+                        return elem;
+                    }
+                }
+
+                // If selector is a data attribute
+                if (firstChar === '[') {
+                    if (elem.hasAttribute(attribute[0])) {
+                        if (value) {
+                            if (elem.getAttribute(attribute[0]) === attribute[1]) {
+                                return elem;
+                            }
+                        } else {
+                            return elem;
+                        }
+                    }
+                }
+
+                // If selector is a tag
+                if (elem.tagName.toLowerCase() === selector) {
+                    return elem;
+                }
+
+            }
+
+            return null;
+
+        },
     },
     qptExtensions = {
         el: {
             target: '.mainView',
-            targetTile: '.bob-title'
+            targetTile: '.bob-title',
+            parentsContainer: '.bob-container'
         },
         cls: {
             activeted: 'activeted'
         },
-        set: function( element ){
+        set: function (element) {
             var _t = this;
-            if( !utils.hasClass({ element: element, cls: _t.cls.activeted }) ){
+            if (!utils.hasClass({ element: element, cls: _t.cls.activeted })) {
                 element.classList.add(_t.cls.activeted);
-                console.log(element.textContent);
+
+                try {
+
+                    var prts = utils.getParents(element, _t.el.parentsContainer),
+                        sib = prts.previousElementSibling,
+                        target = sib.querySelector('a');
+
+                    // netflix idsi alınıp gonderilecek    
+                    console.log(target, element, element.textContent);
+
+
+                } catch (error) {
+                    console.warn(error)
+                }
             }
         },
         addEvent: function () {
@@ -56,13 +210,14 @@ const MutationObserver = window.MutationObserver || window.WebKitMutationObserve
                     });
 
                     if (title)
-                        _t.set(title);    
+                        _t.set(title);
                 });
 
-            observer.observe(target, {
-                childList: true,
-                subtree: true,
-            });
+            if (observer)
+                observer.observe(target, {
+                    childList: true,
+                    subtree: true,
+                });
 
         },
         init: function () {
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 ### resource
+https://developer.chrome.com/extensions/getstarted
 https://www.smashingmagazine.com/2019/04/mutationobserver-api-guide/
 https://developer.mozilla.org/tr/docs/Web/API/MutationObserver
